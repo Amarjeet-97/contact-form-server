93224313ba809249e1904877965fd386e98ce8ee
Amarjeet-97 <106522356+Amarjeet-97@users.noreply.github.com>
2023-09-15 15:05:13 +0530

    Added email

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -2,6 +2,7 @@ var con = require("../db_connection");
 var connection = con.getConnection();
 connection.connect();
 var express = require("express");
+
 var router = express.Router();
 router.get("/", (req, res) => {
   connection.query("select * from contact", (error, array, fields) => {
--- insert/insert.js
+++ insert/insert.js
@@ -1,7 +1,10 @@
+const nodemailer = require("nodemailer");
+
 var con = require("../db_connection");
 var connection = con.getConnection();
 connection.connect();
 var express = require("express");
+
 var router = express.Router();
 router.post("/", (req, res) => {
   var name = req.body.customerName;
@@ -16,6 +19,29 @@ router.post("/", (req, res) => {
       if (err) {
         res.send({ insert: "fail", error: err });
       } else {
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: "amarjeetkumardmg8409@gmail.com",
+            pass: "xmklxzwxtbphwbuu",
+          },
+        });
+        var mailOptions = {
+          from: "amarjeetkumardmg8409@gmail.com",
+          to: req.body.email,
+          cc: "amarjeetkumardmg8409@gmail.com",
+          subject: "Thanks! for feeling contact form.",
+          text: `Thanks for your message you have sent to us. We will contact you soon! Your detail have been saved with us.  \n
+            Name: ${name}\n
+            Contact:${contact}\n
+            Email:${email}\n
+            Address:${Address}`,
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          }
+        });
         res.send({ insert: "success" });
       }
     }
